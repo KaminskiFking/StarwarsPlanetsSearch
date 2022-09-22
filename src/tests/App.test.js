f6114e17c9b9f2b9154f08b43bb8be71859341cc
Vinicius Kaminski <vikaminski@hotmail.com>
2022-09-22 19:03:00 -0300

    Requisito 5 and  60% tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,83 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import mockResponse from './mock';
 import App from '../App';
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Verifica Componentes Filter', () => {
+
+  beforeEach(() => {
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockResponse)
+  })
+  });
+
+  it('Verifica name-filter', () => {
+    render(<App />);
+
+    const nameFilter = screen.getByTestId('name-filter')
+    expect(nameFilter).toBeInTheDocument()
+    userEvent.type(nameFilter, 'Alderaan')
+
+  })
+
+  it('Verifica column-filter', () => {
+    render(<App />);
+
+    const columnFilter = screen.getByTestId('column-filter')
+    expect(columnFilter).toBeInTheDocument()
+    userEvent.selectOptions(columnFilter, 'population')
+
+  })
+
+  it('Verifica comparison-filter', () => {
+    render(<App />);
+
+    const comparisonFilter = screen.getByTestId('comparison-filter')
+    expect(comparisonFilter).toBeInTheDocument()
+    userEvent.selectOptions(comparisonFilter, 'igual a')
+
+  })
+
+  it('Verifica value-filter', () => {
+    render(<App />);
+    const valueFilter = screen.getByTestId('value-filter')
+    expect(valueFilter).toBeInTheDocument()
+    userEvent.type(valueFilter, '10000')
+  })
+
+  it('Verifica button-filter', () => {
+    render(<App />);
+    const buttonFilter = screen.getByTestId('button-filter')
+    expect(buttonFilter).toBeInTheDocument()
+    userEvent.click(buttonFilter)
+
+  })
+
+  it('Verifica evento na tela', async () => {
+    render(<App />);
+
+
+      const columnFilter = screen.getByTestId('column-filter')
+      expect(columnFilter).toBeInTheDocument()
+      userEvent.selectOptions(columnFilter, 'population')
+
+      const comparisonFilter = screen.getByTestId('comparison-filter')
+      expect(comparisonFilter).toBeInTheDocument()
+      userEvent.selectOptions(comparisonFilter, 'igual a')
+
+      const valueFilter = screen.getByTestId('value-filter')
+      expect(valueFilter).toBeInTheDocument()
+      userEvent.type(valueFilter, '2000000000')
+
+      const buttonFilter = screen.getByTestId('button-filter')
+      expect(buttonFilter).toBeInTheDocument()
+      userEvent.click(buttonFilter)
+
+      const resultText = await screen.findByText(/Alderaan/i)
+      expect(resultText).toBeInTheDocument()
+    })
+    
+})
+
+
